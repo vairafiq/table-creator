e8b25b8a75c4ce38a87c2a463e6d2a2746c07914
vairafiq <vairafiq@gmail.com>
2022-08-12 23:57:38 +0600

    improved

--- views/import/google-sheet.php
+++ views/import/google-sheet.php
@@ -6,7 +6,7 @@ $tables = (!empty($args['tables']) && is_array($args['tables'])) ? $args['tables
     <div class="row">
         <div class="col-md-5 col-md-offset-3">
             <div class="attc_table_short_code alert alert-info">
-            <i class="glyphicon glyphicon-shopping-cart"></i><a href="https://exlac.com/product/tablegen-google-sheet-integration"> Get the Google Sheet Integration</a>
+            <i class="glyphicon glyphicon-shopping-cart"></i><a target="_blank" href="https://exlac.com/product/tablegen-google-sheet-integration"> Get the Google Sheet Integration</a>
             </div>
         </div>
     </div>
